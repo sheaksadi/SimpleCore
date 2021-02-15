101f0df300987ac295fce119de55b19facce2dff
sheaksadi <sheaksadi123456@gmail.com>
2021-02-15 21:26:55 +0600

    a

---
+++ src/me/sheak/sadi/Core/Commands.java
@@ -0,0 +1,9 @@
+package me.sheak.sadi.Core;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Commands  {
+
+}
---
+++ src/me/sheak/sadi/Core/Main.java
@@ -0,0 +1,27 @@
+package me.sheak.sadi.Core;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }    public boolean onCommand(CommandSender sender, Command cmd,String str,String[] args){
+        if(str.equalsIgnoreCase("discord")){
+            if(sender instanceof Player){
+                sender.sendMessage(ChatColor.GREEN +"https://discord.gg/Sb4ZNqvC");
+                return true;
+            }
+        }
+        return false;
+    }
+}
